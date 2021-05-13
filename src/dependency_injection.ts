f9856a40498b3a7421cefa8d4da57ef7b4185641
Yamin Nather <2001s.yn@gmail.com>
2021-05-14 03:07:20 +0530

    Supabase repository done.

--- src/dependency_injection.ts
+++ src/dependency_injection.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata";
 import { container } from "tsyringe";
 import SQLiteLanguageRepository from "./language_bounded_context/infrastructure/sqlite_language_repository";
+import SupabaseLanguageRepository from "./language_bounded_context/infrastructure/supabase_language_repository";
 
 export default function gfdependencyInjection(): void {
-    container.register("ILanguageRepository", {useValue: new SQLiteLanguageRepository()});
+    // container.register("ILanguageRepository", {useValue: new SQLiteLanguageRepository()});
+    container.register("ILanguageRepository", {useValue: new SupabaseLanguageRepository()});
 }
